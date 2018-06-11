fec959bffc951937fb0bd7fea665a0c72bea0ae3
uncleLian <771674109@qq.com>
2018-06-11 11:10:38 +0800

    update request.js
    把自定义instance请求的方法和封装的GET/POST方法 进行统一，均返回axios.data 且是promise对象方便业务层处理逻辑。

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
-import qs from 'qs'
+// import qs from 'qs'
 import { Message } from 'element-ui'
 import cache from '@/utils/cache'
 
 // 创建axios实例
 export const instance = axios.create({
-    baseURL: process.env.BASE_API, // api的base_url
+    baseURL: process.env.BASE_API, // 根据配置环境的改变的baseURL（/config目录下进行修改）；不需要则直接注释或者改为自定义URL
     timeout: 10 * 1000 // 请求超时时间
 })
 
@@ -14,6 +14,7 @@ instance.interceptors.request.use(config => {
     // Do something before request is sent
     if (cache.getToken()) {
         config.headers['X-Token'] = cache.getToken() // 让每个请求携带token -- ['X-Token']为自定义key 请根据实际情况自行修改
+        // config.headers['Authorization'] = `Bearer ${cache.getToken()}` // 示例
     }
     return config
 }, error => {
@@ -24,10 +25,10 @@ instance.interceptors.request.use(config => {
 // respone拦截器
 instance.interceptors.response.use(response => {
     const res = response.data
-    if (res && res.error) {
-        return Promise.reject(res.error)
+    if (res && res.errorMessage) {
+        return Promise.reject(res.errorMessage)
     }
-    return response
+    return Promise.resolve(res)
 }, error => {
     Message({
         message: error.message,
@@ -44,12 +45,14 @@ export const request = async (url = '', type = 'GET', data = {}) => {
     if (type === 'GET') {
         await instance.get(url, { params: data })
             .then(res => {
-                result = res.data
+                result = res
             })
     } else if (type === 'POST') {
-        await instance.post(url, qs.stringify(data))
+        // await instance.post(url, qs.stringify(data))  // 发送json格式需转字符串的场景下可打开
+        // 根据反馈，post请求的qs转字符有时候会发生不必要的麻烦（场景：发送表单格式和无需转字符串的请求）
+        await instance.post(url, data)
             .then(res => {
-                result = res.data
+                result = res
             })
     }
     return result
